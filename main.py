0e639a77b4ab7a7590fb16fb2d83b9298abace45
Alexander Lodolo <alexander.lodolo@telecomnancy.eu>
2021-01-19 09:32:23 +0100

    [ADD] Simplex

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 # import Classes
 from Classes.Appliance import Appliance
+from scipy.optimize import linprog
+
+
+#symplex optimizer :https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.linprog.html
+"""
+minimize:
+
+c @ x
+
+such that:
+
+A_ub @ x <= b_ub
+A_eq @ x == b_eq
+lb <= x <= ub
+
+"""
 
 
 print("hello")
