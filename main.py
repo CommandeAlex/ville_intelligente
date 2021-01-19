62e6c24806b925dbfc8ecce110e7ed2a0c4cbc28
Alexander Lodolo <alexander.lodolo@telecomnancy.eu>
2021-01-19 09:07:50 +0100

    [ADD] Dir

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("hello")
+#TO DO: Implementer les classes
+#TO DO: Trouver un solveur de symplex et implementer les variables
+#TO DO: Explorer les datas
+#TO DO: Implementer un data feeder
+#TO DO: Debbug and run the algorithm
+#TO DO: Display results -> classe résultats
+
