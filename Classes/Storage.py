288acc012c362c6b04aa5f297f6c144e6aabbec0
Alexander Lodolo <alexander.lodolo@telecomnancy.eu>
2021-01-19 09:16:02 +0100

    [ADD] Classes

---
+++ Classes/Storage.py
@@ -0,0 +1,12 @@
+class Storage:
+    def __init__(self,E,IE,max_ck,min_ck,SD,SP,list_BE,list_IC,list_SE):
+        self.E = E
+        self.IE = IE
+        self.max_ck = max_ck
+        self.min_ck = min_ck
+        self.SD = SD
+        self.SP = SP
+        self.list_BE = list_BE
+        self.list_IC = list_IC
+        self.list_SE = list_SE
+        pass
