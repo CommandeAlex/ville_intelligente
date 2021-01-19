cc953c26d66f6af82ac2fcd334387da9c9268939
Alexander Lodolo <alexander.lodolo@telecomnancy.eu>
2021-01-19 09:23:35 +0100

    [ADD] Classes 2

---
+++ Classes/EnergySuplier.py
@@ -0,0 +1,4 @@
+class EnergySuplier:
+    def __init__(self,list_GP):
+        self.list_GP = list_GP
+        pass
---
+++ Classes/Household.py
@@ -0,0 +1,15 @@
+class Household:
+    def __init__(self,id,C,C_NT,L_max,list_DS,list_GE,list_MQ,list_RE,list_ME,list_appliance,list_storage,list_renewable):
+        self.id = id
+        self.C = C
+        self.C_NT = C_NT
+        self.L_max = L_max
+        self.list_DS = list_DS
+        self.list_GE = list_GE
+        self.list_MQ = list_MQ
+        self.list_RE = list_RE
+        self.list_ME = list_ME
+        self.list_appliance = list_appliance
+        self.list_storage = list_storage
+        self.list_renewable = list_renewable
+        pass
