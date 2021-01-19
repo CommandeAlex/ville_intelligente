8cfa04b5f29651d55055ea751e31a3049c616a6d
MaxBRB <maxime.botreau-roussel-bonneter@telecomnancy.net>
2021-01-19 09:34:19 +0100

    [MOD] ECO-TRADE

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 # import Classes
 from Classes.Appliance import Appliance
 
+print("Hello World.")
 
-print("hello")
-#TO DO: Implementer les classes
-#TO DO: Trouver un solveur de symplex et implementer les variables
-#TO DO: Explorer les datas
-#TO DO: Implementer un data feeder
-#TO DO: Debbug and run the algorithm
-#TO DO: Display results -> classe résultats
 
+# TO DO: Implementer les classes
+# TO DO: Trouver un solveur de symplex et implementer les variables
+# TO DO: Explorer les datas
+# TO DO: Implementer un data feeder
+# TO DO: Debbug and run the algorithm
+# TO DO: Display results -> classe résultats
+
+"""Data"""
+K = 4   # Number of households
+h = 1   # Step
+hl = 60  # length
+H = [i for i in range(0, hl, h)]  # time distribution
+
+"""Classes instantiation"""
+
+# Generator :
+
+# Microgrid :
+
+# Households :
+
+
+"""Algorithm ECO-TRADE"""
+
+# Module 1
+def module1():
+    # Input : ???
+    # Output : CkTrade
+
+    return 0
+
+
+# ECO-TRADE Algorithm
+
+epsilon_count, epsilon_max, Cpre = 0, 0, 0
